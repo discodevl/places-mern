eda1179235a0d3337498e5ff552e644c3c7c8040
discodevl <donato.cutro3@gmail.com>
2022-08-19 17:06:40 +0200

    input validation

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
+import './NewPlace.css';
 
 function NewPlace() {
   return (
-    <div>
-        oioi
-    </div>
+    <form className='place-form'>
+        <Input type="text" label="title" element='input' validators={[VALIDATOR_REQUIRE()]} onInput={()=>{}} errorText="Please enter a valid  title"/>
+    </form>
   )
 }
 
---
+++ src/shared/components/FormElements/Input.js
@@ -0,0 +1,81 @@
+import { useReducer } from "react";
+import { validate } from "../../util/validators";
+import "./Input.css";
+
+function inputReducer(state, action) {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        ...state,
+        value: action.val,
+        isValid: validate(action.val, action.validators),
+      };
+    case 'TOUCH': {
+        return {
+            ...state,
+            isTouched: true
+        }
+    }
+    default:
+      return state;
+  }
+}
+
+function Input({
+  id,
+  label,
+  type,
+  element,
+  placeholder,
+  rows,
+  errorText,
+  validators,
+}) {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: "",
+    isTouched: false,
+    isValid: false,
+  });
+
+  function changeHandler(e) {
+    dispatch({ type: "CHANGE", val: e.target.value, validators: validators });
+  }
+
+  function touchHandler() {
+    dispatch({type: 'TOUCH'})
+  }
+
+  const elementInput =
+    element === "input" ? (
+      <input
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        onChange={changeHandler}
+        onBlur={touchHandler}
+        value={inputState.value}
+      />
+    ) : (
+      <textarea
+        id={id}
+        rows={rows || 3}
+        onChange={changeHandler}
+        onBlur={touchHandler}
+        value={inputState.value}
+      />
+    );
+
+  return (
+    <div
+      className={`form-control ${
+        !inputState.isValid && inputState.isTouched && "form-control--invalid"
+      }`}
+    >
+      <label htmlFor={id}>{label}</label>
+      {elementInput}
+      {!inputState.isValid && inputState.isTouched && <p>{errorText}</p>}
+    </div>
+  );
+}
+
+export default Input;
