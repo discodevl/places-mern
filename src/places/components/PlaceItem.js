4ada97f408045d6322192f9bb6e8b74ed4923f1d
discodevl <donato.cutro3@gmail.com>
2022-08-19 14:32:35 +0200

    map

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,7 +1,9 @@
-import React from "react";
+import { useState } from "react";
 import Card from "../../shared/components/UIElements/Card";
-import Button from '../../shared/components/FormElements/Button';
+import Button from "../../shared/components/FormElements/Button";
+import Modal from "../../shared/components/UIElements/Modal";
 import styles from "./PlaceItem.module.css";
+import Map from "../../shared/components/UIElements/Map";
 
 function PlaceItem({
   id,
@@ -12,24 +14,47 @@ function PlaceItem({
   creatorId,
   coordinates,
 }) {
+  const [showMap, setShowMap] = useState(false);
+
+  function openMapHandler() {
+    setShowMap(true);
+  }
+  function closeMapHandler() {
+    setShowMap(false);
+  }
+
   return (
-    <li className={styles["place-item"]}>
-      <Card className={styles['place-item__content']}>
-        <div className={styles["place-item__image"]}>
-          <img src={image} alt={title} />
+    <>
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={address}
+        contentClass={styles["place-item__modal-content"]}
+        footerClass={styles['place-item__modal-actions']}
+        footer={<Button onClick={closeMapHandler}>Close</Button>}
+      >
+        <div className={styles['map-container']}>
+          <Map center={coordinates} zoom={16} />
         </div>
-        <div className={styles["place-item__info"]}>
-          <h2>{title}</h2>
-          <h3>{address}</h3>
-          <p>{description}</p>
-        </div>
-        <div className={styles["place-item__actions"]}>
-          <Button inverse>View on Map</Button>
-          <Button to={`/places/${id}`}>Edit</Button>
-          <Button danger >Delete</Button>
-        </div>
-      </Card>
-    </li>
+      </Modal>
+      <li className={styles["place-item"]}>
+        <Card className={styles["place-item__content"]}>
+          <div className={styles["place-item__image"]}>
+            <img src={image} alt={title} />
+          </div>
+          <div className={styles["place-item__info"]}>
+            <h2>{title}</h2>
+            <h3>{address}</h3>
+            <p>{description}</p>
+          </div>
+          <div className={styles["place-item__actions"]}>
+            <Button inverse onClick={openMapHandler}>View on Map</Button>
+            <Button to={`/places/${id}`}>Edit</Button>
+            <Button danger>Delete</Button>
+          </div>
+        </Card>
+      </li>
+    </>
   );
 }
 
