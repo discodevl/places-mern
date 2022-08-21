9b4a87ef99580e017549b4554fb1e1ed0a8971ec
discodevl <donato.cutro3@gmail.com>
2022-08-21 15:49:54 +0200

    auth page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import UserPlaces from "./places/pages/UserPlaces";
 import Users from "./user/pages/Users";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path="/:userId/places" exact component={UserPlaces} />
           <Route path="/places/new" exact component={NewPlace} />
           <Route path="/places/:placeId" exact component={UpdatePlace} />
+          <Route path="/auth" exact component={Auth} />
           <Redirect to="/" />
         </Switch>
       </main>
